1a2a223fd66b8b5d1c44a4731ab526335b2907b3
JJayJohnny <jasiek.barczewski@gmail.com>
2024-08-14 22:03:07 +0200

    added option to link device with owner's phone number

--- parseSMS.py
+++ parseSMS.py
@@ -3,12 +3,33 @@ from sendSMS import sendSMS
 from location import getLocation
 import json
 
+CONFIG_FILE = '/home/ASGDR/Anti-theft-GPS-system/config.json'
+
 availableCommands = {
 	'location': 'Sends device current location',
 	'status': 'Sends device current status (battery, connection, etc.)',
 }
 
 def parseSMS(sender, content):
+	f = open(CONFIG_FILE)
+	data = json.load(f)
+	f.close()
+
+	owner_number = data['owner_number']
+	device_secret = data['device_secret']
+
+	if content == device_secret:
+		print("Setting new owner")
+		data['owner_number'] = sender
+		f = open(CONFIG_FILE, 'w')
+		json.dump(data, f)
+		f.close()
+		sendSMS(sender, f"Hello new user, the device has been successfully linked. You can now use my commands: \n"+json.dumps(availableCommands, indent='\n')[2:-1])
+		return
+
+	if sender != owner_number:
+		return
+
 	if content == "location":
 		location = getLocation()
 		sendSMS(sender, f"My location: {location}")
--- sendSMS.py
+++ sendSMS.py
@@ -1,5 +1,5 @@
 import os
 
 def sendSMS(receiver, content):
-    cmd = "gammu-smsd-inject TEXT " + receiver + " -text '"+content+"'"
+    cmd = "gammu-smsd-inject TEXT " + receiver + " -len " + str(len(content)) + " -text '"+content+"'"
     os.system(cmd)
