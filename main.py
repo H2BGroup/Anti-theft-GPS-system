9a6ec6e6c9e0dd32a62d9f13dbafb114a36e1252
JJayJohnny <jasiek.barczewski@gmail.com>
2024-09-01 18:56:48 +0200

    internet is turned off on service start to make sure GPS can setup

--- main.py
+++ main.py
@@ -2,11 +2,17 @@ from receiveSMS import receiveSMS
 from rabbit import checkRabbit
 from location import setupGPS, power_down
 import time
+import os
 
 SCAN_EVERY = 5 #seconds
 PAUSE_DURATION = 1
 
 def main():
+    # make sure internet if off so gps can setup properly
+    os.system("sudo poff rnet")
+    while os.system("ip link show | grep ppp0 > /dev/null") == 0:
+        time.sleep(0.5)
+
     GPS_on = setupGPS()
     if GPS_on != True:
         print("ERROR Couldn't start GPS")
